5e9f465977074773b3645d141da68ef62140b8d4
paragartbook-star <paragartbook@gmail.com>
2025-12-25 19:58:39 +0000

    Fixed entry point path in index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';  // ये लाइन ऐड करो
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.js';
 import './index.css'; // अगर CSS है तो (ऑप्शनल)
 
 if (darkMode) document.documentElement.classList.add('dark');
